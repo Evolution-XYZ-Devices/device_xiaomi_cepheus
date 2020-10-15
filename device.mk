c77ea837094f16858eb50230505db81ccccbdfea
TheScarastic <warabhishek@gmail.com>
2020-10-15 13:18:13 +0000

    overlay: Adjust biometric dialog for 11
    Change-Id: Id8ee895e7fe8bb643f73eaa45116ecd64d697c38
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -363,7 +363,8 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-evolution \
+    $(LOCAL_PATH)/overlay-system
 
 # Power
 PRODUCT_PACKAGES += \
