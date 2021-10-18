1144b3afebcf10b6f783536007f63affca38e740
Lamjidi Othman <lamjiidii1@gmail.com>
2021-10-18 10:15:40 +0000

    cepheus: Light: CleanUP

--- device.mk
+++ device.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_msmnile
+    android.hardware.light@2.0-service.xiaomi_cepheus
 
 # Media
 PRODUCT_PACKAGES += \
