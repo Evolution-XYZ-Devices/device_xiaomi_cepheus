ecd6a46ce32296cca4802d3fac2840ac19e41fcf
Joey Huab <joey@evolution-x.org>
2022-08-16 16:52:51 +0000

    Drop a2dp

--- device.mk
+++ device.mk
@@ -52,7 +52,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.usb.default \
     audio.usbv2.default \
