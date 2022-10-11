798a1ac5be33b1a6947259b05d7bce5770a5098a
Lucas Dupin <dupin@google.com>
2022-10-11 06:38:14 +0000

    cepheus: Disable blurs during app launch

--- device.mk
+++ device.mk
@@ -165,6 +165,7 @@ PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
 
 # Enable blurs, hidden under dev option
 PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0 \
     ro.surface_flinger.supports_background_blur=1 \
     persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1
