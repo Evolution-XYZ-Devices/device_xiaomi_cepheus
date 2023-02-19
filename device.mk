14aa8091069e9f0f857cc80ff3d745f8d2b7586e
chematelegram <chfondia@gmail.com>
2023-02-19 10:35:47 +0000

    cepheus: Build some camera blobs from source
     - Inspired from LineageOS sm8150-common [1] and cepheus [2] trees.
    
     [1]: https://github.com/xiaomi-sm8150-devs/android_device_xiaomi_sm8150-common/
     [2]: https://github.com/xiaomi-sm8150-devs/android_device_xiaomi_cepheus/
    
    Signed-off-by: baalgx <jose@pixelos.net>

--- device.mk
+++ device.mk
@@ -97,9 +97,20 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
+PRODUCT_PACKAGES += \
+    libcamera2ndk_vendor \
     libdng_sdk.vendor \
-    libshim_megvii \
+    libgui_vendor \
+    libstdc++.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
     GCamMGC
+
+PRODUCT_PACKAGES += \
+    libshim_megvii
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
