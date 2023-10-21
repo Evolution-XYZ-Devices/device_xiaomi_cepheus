9ffab25984960fecea1871303a20315702bde0b7
Tim Zimmermann <tim@linux4.de>
2023-10-22 01:19:41 +0200

    cepheus: Build libprotobuf-cpp-full-3.9.1 vendorcompat
     * Our old blobs depends on this older version of protobuf, instead new 21-7.
    
    Change-Id: I8d1fbc40bebf66abea342780b02cda68d64899b1
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -392,6 +392,10 @@ PRODUCT_COPY_FILES += \
 # Product characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
